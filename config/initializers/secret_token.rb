e7afa3fc4611de87d7ee9cbe7a44159a2c0e6c76
Val Milkevich <milkevich@gmail.com>
2011-09-02 19:03:17 +0300

    rails3 app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vtualete::Application.config.secret_token = '1cb4a4b3d2ed5a3b2e041279975802cc83b151bfb3a75ba28b8346e678ff74178210384aa2728064120c549eb489c2ed92073a51095918d60ef171e79273a5fc'
